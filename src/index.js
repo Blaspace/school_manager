b04fc02219365c667f0215f2ce80f5956d34705a
blaspace <js5618171@gmail.com>
2024-09-02 20:40:03 -0700

    hello

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { SchoolProvider } from "./context/SchoolContext";
 import { StudentProvider } from "./context/StudentContext";
 import { TeacherProvider } from "./context/TeacherContext";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
